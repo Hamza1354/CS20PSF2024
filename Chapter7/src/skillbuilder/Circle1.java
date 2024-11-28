3f4ba6894a34eb3f3e49a6d34467c3cda429509c
48051002 <48051002@L815WD220022.edu.cbe.ab.ca>
2024-11-28 09:48:02 -0700

    9:47am

---
+++ Chapter7/src/skillbuilder/Circle1.java
@@ -0,0 +1,77 @@
+package skillbuilder;
+
+public class Circle1 {
+
+	double radius;
+	private static double PI = 3.14;
+	
+	
+	
+	public Circle1() 
+	{
+		radius = 1;
+	}
+	
+	//Constructor method
+	public Circle1(double r) 
+	{
+		radius = r;	
+		
+	}
+	
+	public void setRadius(double r)
+	{
+		radius = r;
+		
+	}
+	
+	public double getRadius()
+	{
+		return radius;
+		
+	}
+	
+	public double area()
+	{
+		double circArea;
+		
+		circArea = PI * radius * radius;
+		
+		return circArea;
+		
+	}
+	
+	public double circumference()
+	{
+		double circ;
+		
+		circ = 2 * PI * radius;
+		
+		return circ;
+	}
+	
+	public boolean equals(Object c)
+	{
+		Circle1 testObj = (Circle1)c;
+		
+		if(testObj.getRadius() == radius)
+		{
+			return(true);
+		}
+		else
+		{
+			return(false);
+		}
+	}
+	
+	public String toString()
+	{
+		String circ;
+		
+		circ = "Circle has radius "+ radius;
+		
+		return(circ);
+	}
+	
+
+}
